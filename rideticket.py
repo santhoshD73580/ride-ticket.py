be1043dafe5fa1f3fd21b36413acd0a5ba763bd0
SANTHOSH Dilli Babu <santhoshsanthosh73580@gmail.com>
2024-08-08 21:19:05 +0530

    Add files via upload

---
+++ rideticket.py
@@ -0,0 +1,25 @@
+# Enter y hight e.g., 1.55
+height = float(input("Enter the height in CM: "))
+age =int(input("enter your age : "))
+bill=0
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+if height >=120:
+    if age <12:
+        bill=5
+        print("child ticket is $5")
+    elif age <18:
+        bill=7
+        print("ticket ammount is $7")
+    else :
+        bill=12
+        print("the ticket value is $12")        
+    photo_pick =input("Are you intrested to take an pick YES/NO ")
+    if photo_pick =="YES":
+        bill +=3
+        print(f"The total Amount is: ${bill}")
+    else:
+        print(f"The total value is: ${bill}")         
+else:
+    print("you could not ride Becouse you are under 120cm")
